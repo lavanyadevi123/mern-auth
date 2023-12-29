688ac7b000c5e16fe8263671e146bc3bc6c401b8
lavanya <lavanya.devi@sirmaindia.com>
2023-12-29 17:40:21 +0530

    create header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,12 +4,14 @@ import { About } from './pages/About';
 import { SignUp } from './pages/SignUp';
 import { Profile } from './pages/Profile';
 import { SignIn } from './pages/Signin';
+import { Header } from './components/Header';
 
 
 
 export default function App() {
   return (
    <BrowserRouter>
+   <Header />
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/about' element={<About />} />
